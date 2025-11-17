8efd72a3413bd0abb5126dac095afb6c72bcac5f
albert <albertprofe@gmail.com>
2025-11-17 13:41:23 +0100

    feat: vaadin hila create project fresh

---
+++ rentingCar-vaadin/src/main/frontend/views/cars.tsx
@@ -0,0 +1,13 @@
+import { ViewConfig } from '@vaadin/hilla-file-router/types.js';
+
+export const config: ViewConfig = { menu: { order: 1, icon: 'line-awesome/svg/car-side-solid.svg' }, title: 'Cars' };
+
+export default function CarsView() {
+  return (
+    <div className="flex flex-col h-full items-center justify-center p-l text-center box-border">
+      <img style={{ width: '200px' }} src="images/empty-plant.png" />
+      <h2>This place intentionally left empty</h2>
+      <p>It’s a place where you can grow your own UI 🤗</p>
+    </div>
+  );
+}
