60d8a57cfd3e2555a2a714c63b8c84ea345ce73f
nil.sanca97 <nil.sanca97@gmail.com>
2025-11-23 23:52:34 +0100

    modify BookingEndpoint: same as CarEndpoint (look PRA04 code example &  CarEndpoint example from albert repo)

---
+++ rentingCarTest/rentingCar-vaadin/src/main/java/dev/app/rentingcartestvaadin/controller/BookingEndpoint.java
@@ -0,0 +1,28 @@
+package dev.app.rentingcartestvaadin.controller;
+
+import com.vaadin.flow.server.auth.AnonymousAllowed;
+import com.vaadin.hilla.Endpoint;
+import dev.app.rentingcartestvaadin.model.Booking;
+import dev.app.rentingcartestvaadin.service.BookingService;
+import org.springframework.beans.factory.annotation.Autowired;
+
+@Endpoint
+@AnonymousAllowed
+public class BookingEndpoint {
+
+    /*@Autowired
+    BookingService bookingService;
+
+    public Iterable<Booking> getAllBookings() {
+        return bookingService.findAll();
+    }*/
+    private final BookingService bookingService;
+
+    public BookingEndpoint(BookingService bookingService) {
+        this.bookingService = bookingService;
+    }
+
+    public List<Booking> getAllBookings() {
+        return bookingService.findAll();
+    }
+}
